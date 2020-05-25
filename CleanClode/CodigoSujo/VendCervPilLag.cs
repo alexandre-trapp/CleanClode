97bdc5c83c2bda30721a1e7357c82f193777e707
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-05-25 19:04:50 -0300

    add summary

--- CleanClode/CodigoSujo/VendCervPilLag.cs
+++ CleanClode/CodigoSujo/VendCervPilLag.cs
@@ -4,6 +4,18 @@ namespace CleanClode.CodigoSujo
 {
     public static class VendCervPilLag
     {
+        /// <summary>
+        /// Função que vende as cervejas
+        /// por tipo, preço unitario e quantidade
+        /// com os tipos pilsen e lagger, retornando 
+        /// o valor da venda atravé da multiplicação
+        /// do preço unitário * quantidade
+        /// </summary>
+        /// <param name="tpCerv"></param>
+        /// <param name="prcPilUn"></param>
+        /// <param name="prcLagUn"></param>
+        /// <param name="qt"></param>
+        /// <returns></returns>
         public static decimal VdCerv(int tpCerv, decimal prcPilUn, decimal prcLagUn, int qt)
         {
             if (tpCerv == 1)
